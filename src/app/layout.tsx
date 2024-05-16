f77177b4f964f7a2d139f89f0afc657af7431849
fadilmf <128146609+fadilmf@users.noreply.github.com>
2024-05-16 13:48:53 +0700

    remove dangerouslySetInnerHTML

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,20 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/6644290a07f59932ab3fa3e9/1htt40kcl';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
-            })();
-          `,
-          }}
+        <Script
+          strategy="lazyOnload"
+          src="https://embed.tawk.to/6644290a07f59932ab3fa3e9/1htt40kcl"
         />
       </body>
     </html>
